eeaa021d37fe9a27fbf45adafdd2d0330c80e379
Mikhail Mishin <mishinma1805@gmail.com>
2016-06-14 14:43:37 +0300

    add runslow option to tests

--- carsus/conftest.py
+++ carsus/conftest.py
@@ -34,6 +34,37 @@ from sqlalchemy.orm import Session
 # except NameError:   # Needed to support Astropy <= 1.0.0
 #     pass
 
+def pytest_addoption(parser):
+    parser.addoption("--remote-data", action="store_true",
+                     help="run tests with online data")
+    parser.addoption("--open-files", action="store_true",
+                     help="fail if any test leaves files open")
+
+    parser.addoption("--doctest-plus", action="store_true",
+                     help="enable running doctests with additional "
+                     "features not found in the normal doctest "
+                     "plugin")
+
+    parser.addoption("--doctest-rst", action="store_true",
+                     help="enable running doctests in .rst documentation")
+
+    parser.addini("doctest_plus", "enable running doctests with additional "
+                  "features not found in the normal doctest plugin")
+
+    parser.addini("doctest_norecursedirs",
+                  "like the norecursedirs option but applies only to doctest "
+                  "collection", type="args", default=())
+
+    parser.addini("doctest_rst",
+                  "Run the doctests in the rst documentation",
+                  default=False)
+
+    parser.addoption('--repeat', action='store',
+                     help='Number of times to repeat each test')
+
+    parser.addoption("--runslow", action="store_true",
+                     help="include running slow tests during run")
+
 data_dir = os.path.join(os.path.dirname(__file__), 'tests', 'data')
 if not os.path.exists(data_dir):
     os.makedirs(data_dir)
--- carsus/io/tests/test_chianti_io.py
+++ carsus/io/tests/test_chianti_io.py
@@ -4,6 +4,11 @@ from carsus.model import Level, LevelEnergy, Ion, Line, ECollision
 from numpy.testing import assert_almost_equal
 
 
+slow = pytest.mark.skipif(
+    not pytest.config.getoption("--runslow"),
+    reason="need --runslow option to run"
+)
+
 @pytest.fixture(scope="module")
 def ch_ion_reader():
     return ChiantiIonReader("ne_2")
@@ -25,7 +30,7 @@ def test_chianti_reader_read_levels(ch_ion_reader, level_index, energy, energy_t
     assert_almost_equal(row['energy'], energy)
     assert_almost_equal(row['energy_theoretical'], energy_theoretical)
 
-
+@slow
 @pytest.mark.parametrize("atomic_number, ion_charge, levels_count",[
     (10, 1, 138),
     (17, 3, 5)
@@ -35,7 +40,7 @@ def test_chianti_ingest_levels_count(test_session, ch_ingester, atomic_number, i
     ion = Ion.as_unique(test_session, atomic_number=atomic_number, ion_charge=ion_charge)
     assert len(ion.levels) == levels_count
 
-
+@slow
 @pytest.mark.parametrize("atomic_number, ion_charge, lines_count",[
     (10, 1, 1999)
 ])
@@ -45,7 +50,7 @@ def test_chianti_ingest_lines_count(test_session, ch_ingester, atomic_number, io
     cnt = test_session.query(Line).join(Line.lower_level).filter(Level.ion==ion).count()
     assert cnt == lines_count
 
-
+@slow
 @pytest.mark.parametrize("atomic_number, ion_charge, e_col_count",[
     (10, 1, 9453)
 ])
