c9fb97fc9114e472a2c0d68bfef88c14b1c09ecc
Mikhail Mishin <mishinma1805@gmail.com>
2016-07-05 15:58:48 +0300

    test created instead of prepared

--- carsus/io/tests/test_output_tardis.py
+++ carsus/io/tests/test_output_tardis.py
@@ -21,38 +21,38 @@ def atom_data(test_session):
 
 
 @pytest.fixture
-def atom_masses_prepared(atom_data):
-    return atom_data.atom_masses_prepared
+def atom_masses(atom_data):
+    return atom_data.atom_masses
 
 
 @pytest.fixture
-def ionization_energies_prepared(atom_data):
-    return atom_data.ionization_energies_prepared
+def ionization_energies(atom_data):
+    return atom_data.ionization_energies
 
 
 @pytest.fixture
-def levels_prepared(atom_data):
-    return atom_data.levels_prepared
+def levels(atom_data):
+    return atom_data.levels
 
 
 @pytest.fixture
-def lines_prepared(atom_data):
-    return atom_data.lines_prepared
+def lines(atom_data):
+    return atom_data.lines
 
 
 @pytest.fixture
-def collisions_prepared(atom_data):
-    return atom_data.collisions_prepared
+def collisions(atom_data):
+    return atom_data.collisions
 
 
 @pytest.fixture
-def macro_atom_prepared(atom_data):
-    return atom_data.macro_atom_prepared
+def macro_atom(atom_data):
+    return atom_data.macro_atom
 
 
 @pytest.fixture
-def macro_atom_references_prepared(atom_data):
-    return atom_data.macro_atom_references_prepared
+def macro_atom_references(atom_data):
+    return atom_data.macro_atom_references
 
 
 @pytest.fixture
@@ -71,25 +71,26 @@ def hdf5_path(request, data_dir):
     (2, 4.002602),
     (11, 22.98976928)
 ])
-def test_prepare_atom_masses(atom_masses_prepared, atomic_number, exp_mass):
-    assert_almost_equal(atom_masses_prepared.loc[atomic_number]["mass"], exp_mass)
+def test_create_atom_masses(atom_masses, atomic_number, exp_mass):
+    atom_masses = atom_masses.set_index("atomic_number")
+    assert_almost_equal(atom_masses.loc[atomic_number]["mass"], exp_mass)
 
 
 @with_test_db
-def test_prepare_atom_masses_max_atomic_number(test_session):
+def test_create_atom_masses_max_atomic_number(test_session):
     atom_data = AtomData(test_session, atom_masses_max_atomic_number=15)
-    atom_masses = atom_data.atom_masses_prepared
-    atom_masses.reset_index(inplace=True)
+    atom_masses = atom_data.atom_masses
     assert atom_masses["atomic_number"].max() == 15
 
 
 @with_test_db
 @pytest.mark.parametrize("atomic_number, ion_number, exp_ioniz_energy", [
-    (8, 6, 138.1189),
-    (11, 1,  5.1390767)
+    (8, 5, 138.1189),
+    (11, 0,  5.1390767)
 ])
-def test_prepare_ionizatinon_energies(ionization_energies_prepared, atomic_number, ion_number, exp_ioniz_energy):
-    assert_almost_equal(ionization_energies_prepared.loc[(atomic_number, ion_number)]["ionization_energy"],
+def test_create_ionizatinon_energies(ionization_energies, atomic_number, ion_number, exp_ioniz_energy):
+    ionization_energies = ionization_energies.set_index(["atomic_number", "ion_number"])
+    assert_almost_equal(ionization_energies.loc[(atomic_number, ion_number)]["ionization_energy"],
                         exp_ioniz_energy)
 
 
@@ -98,9 +99,9 @@ def test_prepare_ionizatinon_energies(ionization_energies_prepared, atomic_numbe
     (7, 5, 7, 3991860.0 * u.Unit("cm-1")),
     (4, 2, 2, 981177.5 * u.Unit("cm-1"))
 ])
-def test_prepare_levels(levels_prepared, atomic_number, ion_number, level_number, exp_energy):
-    levels_prepared.set_index(["atomic_number", "ion_number", "level_number"], inplace=True)
-    energy = levels_prepared.loc[(atomic_number, ion_number, level_number)]["energy"] * u.eV
+def test_create_levels(levels, atomic_number, ion_number, level_number, exp_energy):
+    levels = levels.set_index(["atomic_number", "ion_number", "level_number"])
+    energy = levels.loc[(atomic_number, ion_number, level_number)]["energy"] * u.eV
     energy = energy.to(u.Unit("cm-1"), equivalencies=u.spectral())
     assert_quantity_allclose(energy, exp_energy)
 
@@ -110,28 +111,28 @@ def test_prepare_levels(levels_prepared, atomic_number, ion_number, level_number
     (7, 5, 1, 2, 1907.9000 * u.Unit("angstrom")),
     (4, 2, 0, 6, 10.0255 * u.Unit("angstrom"))
 ])
-def test_prepare_lines(lines_prepared, atomic_number, ion_number,
+def test_create_lines(lines, atomic_number, ion_number,
                        level_number_lower, level_number_upper, exp_wavelength):
-    lines_prepared.set_index(["atomic_number", "ion_number",
-                              "level_number_lower", "level_number_upper"], inplace=True)
-    wavelength = lines_prepared.loc[(atomic_number, ion_number,
+    lines = lines.set_index(["atomic_number", "ion_number",
+                              "level_number_lower", "level_number_upper"])
+    wavelength = lines.loc[(atomic_number, ion_number,
                                         level_number_lower, level_number_upper)]["wavelength"] * u.Unit("angstrom")
     assert_quantity_allclose(wavelength, exp_wavelength)
 
 
 # ToDo: Implement real tests
 @with_test_db
-def test_prepare_collisions_df(collisions_prepared):
+def test_create_collisions_df(collisions):
     assert True
 
 
 @with_test_db
-def test_prepare_macro_atom_df(macro_atom_prepared):
+def test_create_macro_atom_df(macro_atom):
     assert True
 
 
 @with_test_db
-def test_prepare_macro_atom_ref_df(macro_atom_references_prepared):
+def test_create_macro_atom_ref_df(macro_atom_references):
     assert True
 
 
