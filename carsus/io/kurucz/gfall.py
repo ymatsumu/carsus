f0e88d6aa15b1acec31fcc5f581d89f8d2860d3a
Mikhail Mishin <mishinma1805@gmail.com>
2016-07-20 18:58:17 +0300

    Sort first then drop duplicates

--- carsus/io/kurucz/gfall.py
+++ carsus/io/kurucz/gfall.py
@@ -206,8 +206,8 @@ class GFALLReader(object):
         levels = pd.concat([e_lower_levels[selected_columns],
                             e_upper_levels[selected_columns]])
 
-        levels = levels.drop_duplicates(['atomic_number', 'ion_charge', 'energy', 'j']). \
-            sort_values(['atomic_number', 'ion_charge', 'energy', 'j'])
+        levels = levels.sort_values(['atomic_number', 'ion_charge', 'energy', 'j']).\
+            drop_duplicates(['atomic_number', 'ion_charge', 'energy', 'j'])
 
         levels["method"] = levels["theoretical"].\
             apply(lambda x: "theor" if x else "meas")  # Theoretical or measured
