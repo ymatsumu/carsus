0a7f7042eef6ea9deaf339024c19d9ac5065fa52
Mikhail Mishin <mishinma1805@gmail.com>
2016-08-11 14:03:29 +0300

    Remove relative imports

--- carsus/model/__init__.py
+++ carsus/model/__init__.py
@@ -1,5 +1,5 @@
-from .atomic import Atom, AtomQuantity, AtomWeight, DataSource,\
+from carsus.model.atomic import Atom, AtomQuantity, AtomWeight, DataSource,\
     Ion, IonQuantity, IonizationEnergy, LevelEnergy, Level, LevelQuantity,\
     Transition, Line, LineQuantity, LineAValue, LineWavelength, LineGFValue,\
     ECollision, ECollisionQuantity, ECollisionGFValue, ECollisionEnergy, ECollisionTempStrength
-from .meta import Base, setup
+from carsus.model.meta import Base, setup
