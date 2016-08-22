3e1fe56ad9426594b55fe79ae2648b9d59c864d4
Mikhail Mishin <mishinma1805@gmail.com>
2016-08-22 16:01:29 +0300

    Implement grammar for parsing species

--- carsus/util/selected.py
+++ carsus/util/selected.py
@@ -4,11 +4,20 @@ BNF grammar for parsing spectra
 element ::= 'H' .. 'Uuh'
 element_range ::= element + '-' + element
 
-selected_atoms = element | element_range + [',' + element | element_range]*
+atom_entry = element | element_range
+selected_atoms = atom_entry + [',' + atom_entry]*
+
+ion_number ::= decimal
+ion_number_range ::= ion_number - ion_number
+ion_numbers = ion_number | ion_number_range + [',' + ion_number | ion_number_range]
+
+species_entry =  selected_atoms + [ion_numbers]
+selected_species = species_entry + [';' + species_entry]*
 """
 
 from helpers import symbol2atomic_number
-from pyparsing import Literal, Suppress, delimitedList, Word, alphas
+from pyparsing import Literal, Suppress, delimitedList,\
+    Word, alphas, nums, Optional, Group
 
 hyphen = Suppress(Literal('-'))
 
@@ -29,17 +38,52 @@ element.setParseAction(parse_element)
 element_range = element + hyphen + element
 element_range.setParseAction(lambda x: range(x[0], x[1] + 1))
 
-selected_atomic_numbers = delimitedList(element ^ element_range)
-selected_atomic_numbers.setParseAction(lambda x: sorted(set(x)))
+atom_entry = element ^ element_range
+selected_atoms = delimitedList(atom_entry)
+selected_atoms.setParseAction(lambda x: sorted(set(x)))
 
 
-def parse_selected_atoms(selected_atoms):
+ion_number = Word(nums)
+ion_number.setParseAction(lambda x: int(x[0]))
+
+ion_number_range = ion_number + hyphen + ion_number
+ion_number_range.setParseAction(lambda x: range(x[0], x[1] + 1))
+
+ion_numbers = delimitedList(ion_number ^ ion_number_range)
+ion_numbers.setParseAction(lambda x: sorted(set(x)))
+
+species_entry = Group(selected_atoms).setResultsName('atomic_numbers') + \
+                Group(Optional(ion_numbers)).setResultsName('ion_numbers')
+
+
+def parse_species_entry(tokens):
+    species = list()
+
+    if tokens['ion_numbers']:
+        species = [(atomic_number, ion_number)
+                   for atomic_number in tokens['atomic_numbers']
+                   for ion_number in tokens['ion_numbers']
+                   if atomic_number > ion_number]
+    else:
+        species = [(atomic_number, ion_number)
+                   for atomic_number in tokens['atomic_numbers']
+                   for ion_number in range(atomic_number)]
+
+    return species
+
+species_entry.setParseAction(parse_species_entry)
+
+selected_species = delimitedList(species_entry, delim=';')
+selected_species.setParseAction(lambda x: sorted(set(x)))
+
+
+def parse_selected_atoms(atoms):
     """
     Parse the sting specifying selected atoms to the list of atomic numbers.
 
     Parameters
     ----------
-    selected_atoms: str
+    atoms: str
         Sting that specifies selected atoms. It should consist of comma-separated entries
         that are either single atoms (e.g. "H") or ranges (indicated by using a hyphen between, e.g "H-Zn").
         Element symbols need **not** to be capitalized.
@@ -62,5 +106,45 @@ def parse_selected_atoms(selected_atoms):
     [1, 3, 4, 5, 6, 7, 14, 16]
 
     """
-    return selected_atomic_numbers.parseString(selected_atoms).asList()
+    return selected_atoms.parseString(atoms).asList()
 
+
+def parse_selected_species(species):
+    """
+    Parse the sting specifying selected species to the list of tuples in the
+    form (atomic_number, ion_number).
+
+    Parameters
+    ----------
+    species: str
+        Sting that specifies selected species. It should consist of semicolon-separated entries.
+        The entries can be just element symbols or ranges of elements symbols. In this case
+        all ions of the elements are selected. Also, ion numbers (starting from 0)
+        can be specified - either as numbers or ranges. In this case only ions that have
+        the corresponding ionization stage are selected.
+
+    Returns
+    -------
+    list of tuples (atomic_number, ion_number)
+        List of selected ions
+
+    Examples
+    --------
+
+    >>> parse_selected_species('H')
+    [(1, 0)]
+
+    >>> parse_selected_species('H-Li')
+    [(1, 0), (2, 0), (2, 1), (3, 0), (3, 1), (3, 2)]
+
+    >>> parse_selected_species('h-li 0')
+    [(1, 0), (2, 0), (3, 0)]
+
+    >>> parse_selected_species('b 3-5')
+    [(5, 3), (5, 4)]
+
+    >>> parse_selected_species('Li 3; B-O 4-5')
+    [(5, 4), (6, 4), (6, 5), (7, 4), (7, 5), (8, 4), (8, 5)]
+
+    """
+    return selected_species.parseString(species).asList()
