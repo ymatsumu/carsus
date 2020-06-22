07a8e7b7ec8a78320c9bb79f0d81265650053809
Ezequiel Pássaro <epassaro15@gmail.com>
2020-06-22 12:45:20 -0300

    Uncomment import lines (#170)

--- carsus/io/__init__.py
+++ carsus/io/__init__.py
@@ -1,6 +1,6 @@
-#from carsus.io.nist import NISTIonizationEnergiesParser, NISTIonizationEnergiesIngester,\
-#    NISTWeightsCompPyparser, NISTWeightsCompIngester
-#from carsus.io.chianti_ import ChiantiIonReader, ChiantiIngester
-#from carsus.io.kurucz import GFALLReader, GFALLIngester
-#from carsus.io.output import AtomData
-#from carsus.io.zeta import KnoxLongZetaIngester
+from carsus.io.nist import NISTIonizationEnergiesParser, NISTIonizationEnergiesIngester,\
+    NISTWeightsCompPyparser, NISTWeightsCompIngester
+from carsus.io.chianti_ import ChiantiIonReader, ChiantiIngester
+from carsus.io.kurucz import GFALLReader, GFALLIngester
+from carsus.io.output import AtomData
+from carsus.io.zeta import KnoxLongZetaIngester
