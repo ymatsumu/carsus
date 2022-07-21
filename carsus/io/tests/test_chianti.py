2db97e18b435c61472ebc59b90546d216cfd610e
Atharva Arya <55894364+atharva-2001@users.noreply.github.com>
2022-07-21 21:09:05 +0530

    Test Chianti Submodule using Pytest Arraydiff (#312)
    * Test get_seaton_phixs_table
    
    * Add tests for other functions
    
    * Remove unnecessary changes
    
    * Remove 'h' from arguments because it's never used
    
    * Add more tests
    
    * Use pytest arraydiff for testing levels and lines dataframes
    
    * Remove unused fixtures and text blocks
    
    * Change tests for CMFGEN energy levels, oscillation strengths and collisional strength parsers
    
    * Remove unused imports
    
    * Remove path fixtures and instead parameterize test functions
    
    * Remove extra changes
    
    * Try running pytest tests in the tests workflow
    
    Check version pytest arraydiff
    
    Pin arraydiff version
    
    Install pytest arraydiff using pip
    
    Print plugin
    
    Try conda skeleton
    
    Try install pytest arraydiff from Ezequiel's channel
    
    Install pytest arraydiff using pip,git
    
    Change arraydiff branch name when  fetching from git repo
    
    * Remove unnecessary changes from the tests workflow
    
    * Remove unnecessary changes from the env file and upgrade arraydiff version
    
    * Try removing version
    
    * Add documentation for pytest arraydiff options
    
    [build docs]
    
    * Black format
    
    * Tests cross sections and ionization energies, remove data_dict fixture because parsers are already tested
    
    * Provide path to CMFGEN data
    
    Download CMFGEN dataset in the workflow
    
    * Drop mismatched labels
    
    * Convert ionization energies to dataframe
    
    * Remove step for generating reference files
    
    * Add arraydiff options to PYTEST_FLAGS environment variable
    
    * Place the h parameter in get_hummer_phixs_table function back
    
    * Use CMFGEN cache
    
    * Return bound lines, bound levels and reader levels, format the file
    
    * Parametrize ch_ion_reader fixture
    
    * Test read_collisions
    
    * Create a TestChiantiIonReader class
    
    * Create a TestChiantiReader class
    
    * trigger test workflow

--- carsus/io/tests/test_chianti.py
+++ carsus/io/tests/test_chianti.py
@@ -1,80 +1,113 @@
 import pytest
 
 from numpy.testing import assert_almost_equal
-from carsus.io.chianti_ import ChiantiIonReader, ChiantiIngester
+from carsus.io.chianti_ import ChiantiIonReader, ChiantiIngester, ChiantiReader
 from carsus.model import Level, Ion, Line, ECollision
 
 
 slow = pytest.mark.skipif(
-    not pytest.config.getoption("--runslow"),
-    reason="need --runslow option to run"
+    not pytest.config.getoption("--runslow"), reason="need --runslow option to run"
 )
-
-
-@pytest.fixture(scope="module")
-def ch_ion_reader():
-    return ChiantiIonReader("ne_2")
 
 
 @pytest.fixture
 def ch_ingester(memory_session):
-    ions = 'ne 1; cl 3'
+    ions = "ne 1; cl 3"
     ingester = ChiantiIngester(memory_session, ions=ions)
     return ingester
 
 
-@pytest.mark.parametrize("ion_name", ["ne_2", "n_5"])
-def test_chianti_bound_levels(ion_name):
-    ion_rdr = ChiantiIonReader(ion_name)
-    bound_levels = ion_rdr.bound_levels.reset_index()
-    assert bound_levels["level_index"].max() <= ion_rdr.last_bound_level
+class TestChiantiIonReader:
+    @pytest.fixture(scope="class", params=["ne_2", "n_5"])
+    def ch_ion_reader(self, request):
+        return ChiantiIonReader(request.param)
 
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_chianti_bound_levels(self, ch_ion_reader):
+        bound_levels = ch_ion_reader.bound_levels
+        return bound_levels
 
-@pytest.mark.parametrize("ion_name", ["ne_2", "n_5"])
-def test_chianti_bound_lines(ion_name):
-    ion_rdr = ChiantiIonReader(ion_name)
-    bound_lines = ion_rdr.bound_lines.reset_index()
-    assert bound_lines["upper_level_index"].max() <= ion_rdr.last_bound_level
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_chianti_bound_lines(self, ch_ion_reader):
+        bound_lines = ch_ion_reader.bound_lines
+        return bound_lines
 
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_chianti_reader_read_levels(self, ch_ion_reader):
+        return ch_ion_reader.levels
 
-@pytest.mark.parametrize("level_index, energy, energy_theoretical",[
-    (1, 0, 0),
-    (21, 252953.5, 252954),
-])
-def test_chianti_reader_read_levels(ch_ion_reader, level_index, energy, energy_theoretical):
-    row = ch_ion_reader.levels.loc[level_index]
-    assert_almost_equal(row['energy'], energy)
-    assert_almost_equal(row['energy_theoretical'], energy_theoretical)
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_chianti_reader_read_lines(self, ch_ion_reader):
+        return ch_ion_reader.lines
+
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_chianti_reader_read_collisions(self, ch_ion_reader):
+        return ch_ion_reader.collisions
 
 
 @slow
-@pytest.mark.parametrize("atomic_number, ion_charge, levels_count",[
-    (10, 1, 138),
-    (17, 3, 5)
-])
-def test_chianti_ingest_levels_count(memory_session, ch_ingester, atomic_number, ion_charge, levels_count):
+@pytest.mark.parametrize(
+    "atomic_number, ion_charge, levels_count", [(10, 1, 138), (17, 3, 5)]
+)
+def test_chianti_ingest_levels_count(
+    memory_session, ch_ingester, atomic_number, ion_charge, levels_count
+):
     ch_ingester.ingest(levels=True)
-    ion = Ion.as_unique(memory_session, atomic_number=atomic_number, ion_charge=ion_charge)
+    ion = Ion.as_unique(
+        memory_session, atomic_number=atomic_number, ion_charge=ion_charge
+    )
     assert len(ion.levels) == levels_count
 
 
 @slow
-@pytest.mark.parametrize("atomic_number, ion_charge, lines_count",[
-    (10, 1, 1999)
-])
-def test_chianti_ingest_lines_count(memory_session, ch_ingester, atomic_number, ion_charge, lines_count):
+@pytest.mark.parametrize("atomic_number, ion_charge, lines_count", [(10, 1, 1999)])
+def test_chianti_ingest_lines_count(
+    memory_session, ch_ingester, atomic_number, ion_charge, lines_count
+):
     ch_ingester.ingest(levels=True, lines=True)
-    ion = Ion.as_unique(memory_session, atomic_number=atomic_number, ion_charge=ion_charge)
-    cnt = memory_session.query(Line).join(Line.lower_level).filter(Level.ion==ion).count()
+    ion = Ion.as_unique(
+        memory_session, atomic_number=atomic_number, ion_charge=ion_charge
+    )
+    cnt = (
+        memory_session.query(Line)
+        .join(Line.lower_level)
+        .filter(Level.ion == ion)
+        .count()
+    )
     assert cnt == lines_count
 
 
 @slow
-@pytest.mark.parametrize("atomic_number, ion_charge, e_col_count",[
-    (10, 1, 9453)
-])
-def test_chianti_ingest_e_col_count(memory_session, ch_ingester, atomic_number, ion_charge, e_col_count):
+@pytest.mark.parametrize("atomic_number, ion_charge, e_col_count", [(10, 1, 9453)])
+def test_chianti_ingest_e_col_count(
+    memory_session, ch_ingester, atomic_number, ion_charge, e_col_count
+):
     ch_ingester.ingest(levels=True, collisions=True)
-    ion = Ion.as_unique(memory_session, atomic_number=atomic_number, ion_charge=ion_charge)
-    cnt = memory_session.query(ECollision).join(ECollision.lower_level).filter(Level.ion==ion).count()
+    ion = Ion.as_unique(
+        memory_session, atomic_number=atomic_number, ion_charge=ion_charge
+    )
+    cnt = (
+        memory_session.query(ECollision)
+        .join(ECollision.lower_level)
+        .filter(Level.ion == ion)
+        .count()
+    )
     assert cnt == e_col_count
+
+
+class TestChiantiReader:
+    @pytest.fixture(scope="class", params=["H-He", "N"])
+    def ch_reader(self, request):
+        return ChiantiReader(ions=request.param, collisions=True, priority=20)
+
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_levels(self, ch_reader):
+        return ch_reader.levels
+
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_lines(self, ch_reader):
+        return ch_reader.lines
+
+    @pytest.mark.array_compare(file_format="pd_hdf")
+    def test_cols(self, ch_reader):
+        return ch_reader.collisions
