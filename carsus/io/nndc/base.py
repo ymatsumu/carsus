13bda77af25e1f375dbba9040c87a813e47e64f7
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2023-06-12 10:07:01 -0400

    Include nndc (#320)
    * initial commit and copying over from tardisnuclear
    
    * renaming tardisnuclear -> nuclear
    
    * set unc to nan if missing
    
    * removed print statements
    
    * fix the downloading and storing of decay radiation
    
    * some fixes and adds to the documentation
    
    * fixed uncertainty string
    
    * added addtional metadata (#2)
    
    * added addtional metadata
    
    * formatted uncertainty string
    
    * removed duplicated uncertainty parser
    
    * Merge branch 'extended_metadata' into master
    
    * Update base.py
    
    * Fix parsing exponential notation ('x10') in NNDC web page (#9)
    
    * Merge pull request #4 from tardis-sn/Update-nudat-url
    
    Updated nudat2 to nudat3 in NNDC url
    
    * move nuclear to carsus
    
    ---------
    
    Co-authored-by: Andreas Flörs <33418619+afloers@users.noreply.github.com>
    Co-authored-by: Luke Shingles <luke.shingles@gmail.com>

---
+++ carsus/io/nndc/base.py
@@ -0,0 +1,292 @@
+import urllib.request, urllib.error, urllib.parse
+import os
+import re
+import logging
+from astropy import units as u
+
+logger = logging.getLogger(__name__)
+
+import bs4
+import pandas as pd
+
+import pathlib
+
+# getting the data_path
+
+from nuclear.config import get_data_dir
+from nuclear.io.nndc.parsers import decay_radiation_parsers, uncertainty_parser
+
+TARDISNUCLEAR_DATA_DIR = pathlib.Path(get_data_dir())
+import datetime
+from pyne import nucname
+from uncertainties import ufloat_fromstr
+
+NNDC_DECAY_RADIATION_BASE_URL = (
+    "http://www.nndc.bnl.gov/nudat3/" "decaysearchdirect.jsp?nuc={nucname}&unc=nds"
+)
+
+NNDC_ARTIFICIAL_DATASET_TAG = "NNDC_DATASET_SPLITTER"
+
+
+def _get_nuclear_database_path():
+    if not TARDISNUCLEAR_DATA_DIR.exists():
+        os.mkdir(TARDISNUCLEAR_DATA_DIR)
+    return TARDISNUCLEAR_DATA_DIR / "decay_radiation.h5"
+
+
+def _sanitize_isotope_string(isotope_string):
+    """
+    Checks if the string given is a valid isotope_string
+
+    Parameters
+    ----------
+    isotope_string: str
+
+    Returns
+    -------
+    sanitized_isotope_string: str
+    """
+    try:
+        sanitized_isotope_string = nucname.name(isotope_string)
+    except RuntimeError:
+        raise ValueError(f"{isotope_string} not a valid isotope string")
+    else:
+        return sanitized_isotope_string
+
+
+def construct_decay_radiation_url(isotope_string):
+    """
+    Construct the URL for downloading the decay_radiation
+    Returns
+    -------
+
+    """
+    isotope_string = _sanitize_isotope_string(isotope_string)
+
+    return NNDC_DECAY_RADIATION_BASE_URL.format(nucname=isotope_string.upper())
+
+
+def download_raw_decay_radiation(isotope_string):
+    """
+    Download the dataset from NNDC. Splitup the page into different dataset
+    types. Return a list of dictionaries that contains a dataset for list entry
+    Parameters
+    ----------
+    isotope_string: str
+
+    Returns
+    -------
+    datasets: dict
+    """
+    nndc_data_url = construct_decay_radiation_url(isotope_string)
+    logger.info(f"Downloading data from {nndc_data_url}")
+    nuclear_bs = bs4.BeautifulSoup(
+        urllib.request.urlopen(nndc_data_url), features="lxml"
+    )
+
+    for utag in nuclear_bs.find_all("u"):
+        utag.insert_before(NNDC_ARTIFICIAL_DATASET_TAG)
+
+    split_raw_dataset = str(nuclear_bs).split(NNDC_ARTIFICIAL_DATASET_TAG)
+
+    datasets = []
+    cur_dataset = {}
+    for data_portion in split_raw_dataset[1:]:
+        data_portion = bs4.BeautifulSoup(data_portion, features="lxml")
+        data_type = data_portion.find("u").text
+        if data_type.startswith("Result"):
+            continue
+        if data_type.startswith("Dataset"):
+            if len(cur_dataset) > 0:
+                datasets.append(cur_dataset)
+            cur_dataset = {}
+        cur_dataset[data_type] = str(data_portion)
+    cur_dataset["download-timestamp"] = str(datetime.datetime.utcnow())
+    datasets.append(cur_dataset)
+    return datasets
+
+
+DATASET_PATTERN = re.compile(r"Dataset\s#(\d):")
+
+
+def parse_decay_radiation_dataset(decay_rad_dataset_dict):
+    """
+    Parse decay radiation dataset to dataframes and compile them
+
+    Parameters
+    ----------
+    decay_rad_dataset_dict
+
+    Returns
+    -------
+
+    """
+
+    meta = {
+        "energy_column_unit": u.keV,
+        "end_point_energy_column_unit": u.keV,
+        "intensity_column_unit": u.percent,
+        "intensity_unc_column_unit": u.keV,
+    }
+    dataset = []
+    for data_type, data_portion in decay_rad_dataset_dict.items():
+        if DATASET_PATTERN.match(data_type):
+            data_set_id = int(DATASET_PATTERN.match(data_type).group(1))
+            logger.info(f"Importing new Dataset {data_set_id}")
+            dataset = []
+            if len(dataset) != 0:
+                raise ValueError(
+                    "Trying to import multiple Datasets for one "
+                    "isotope. Currently, this is not supported - "
+                    "please file an issue on GitHub."
+                )
+        elif data_type in decay_radiation_parsers:
+            parser = decay_radiation_parsers[data_type]
+            dataset.append(parser.parse(data_portion))
+        elif data_type == "Authors":
+            author_bs = bs4.BeautifulSoup(data_portion, "lxml")
+            meta["authors"] = author_bs.find("body").text.split(":")[1].strip()
+        elif data_type == "Citation":
+            citation_bs = bs4.BeautifulSoup(data_portion, "lxml")
+            citation_data = (
+                citation_bs.find("body").text.split(":")[1].split("Parent")[0]
+            )
+            meta["citation"] = citation_data[:-1]
+            decay_table = data_portion.split("</p>", 1)[1]
+            decay_table = decay_table.split("<p></p>")[0]
+            decay_table_df = pd.read_html(decay_table)[0]
+            decay_table_df.columns = decay_table_df.iloc[0]
+            decay_table_df = decay_table_df.drop(decay_table_df.index[0])
+            for column in decay_table_df:
+                if column != "DecayScheme" and column != "ENSDFfile":
+                    if column == "Parent T1/2" or column == "GS-GS Q-value (keV)":
+                        value = decay_table_df[column].values[0]
+                        unit = "".join(x for x in value if x.isalpha())
+                        if unit == "":
+                            unit = "\xa0"
+                            value = value.replace("\xa0", " \xa0 ")
+                        nominal, unc = uncertainty_parser(value, split_unc_symbol=unit)
+                        if column == "GS-GS Q-value (keV)":
+                            unit = column[-4:-1]
+                            column = column[:-12]
+                        meta[column + " value"] = str(nominal) + " " + unit
+                        meta[column + " unc"] = str(unc) + " " + unit
+                    else:
+                        meta[column] = decay_table_df[column].values[0]
+        else:
+            logger.warning(f"Data Type {data_type} not known and not parsed")
+    full_dataset = pd.concat(dataset)
+    full_dataset["download-timestamp"] = decay_rad_dataset_dict["download-timestamp"]
+    return full_dataset, meta
+
+
+def download_decay_radiation(isotope_string):
+    """
+    Download and parse decay radiation from NNDC
+
+    Parameters
+    ----------
+    isotope_string
+
+    Returns
+    -------
+
+    """
+    isotope_string = _sanitize_isotope_string(isotope_string)
+    raw_datasets = download_raw_decay_radiation(isotope_string)
+    # print(raw_datasets)
+    decay_radiation, meta = parse_decay_radiation_dataset(raw_datasets[-1])
+    meta = pd.Series(meta).to_frame().reset_index()
+    meta.columns = ["key", "value"]
+    meta["isotope"] = isotope_string
+    meta["value"] = meta["value"].astype(str)
+    decay_radiation["isotope"] = isotope_string
+    return decay_radiation.set_index("isotope"), meta.set_index("isotope")
+
+
+def update_decay_radiation_from_ejecta(ejecta, force_update=False):
+    """
+    Check if all isotopes of a given ejecta are in the database and
+    download if necessary.
+
+    :param ejecta:
+    :param force_update:
+    :return:
+    """
+
+    for isotope in ejecta.get_all_children_nuc_name():
+        print("Working on isotope", isotope)
+        try:
+            store_decay_radiation(isotope, force_update=force_update)
+        except IOError as e:
+            print(str(e))
+            print("skipping")
+
+
+def store_decay_radiation(isotope_string, force_update=False):
+    """
+
+    Parameters
+    ----------
+    isotope_string
+    force_update
+
+    Returns
+    -------
+
+    """
+
+    isotope_string = _sanitize_isotope_string(isotope_string)
+    db_fname = _get_nuclear_database_path()
+    if not db_fname.exists():
+        file_exists = False
+        data_exists = False
+    else:
+        file_exists = True
+        decay_radiation_db = pd.read_hdf(db_fname, "decay_radiation")
+        if isotope_string in decay_radiation_db.index:
+            data_exists = True
+        else:
+            data_exists = False
+
+    if data_exists and not force_update:
+        logger.warning(
+            f"{isotope_string} is already in the database "
+            "(force_update to overwrite)"
+        )
+
+    new_decay_radiation, new_meta = download_decay_radiation(isotope_string)
+
+    if file_exists:
+        decay_radiation = pd.read_hdf(db_fname, "decay_radiation")
+        meta = pd.read_hdf(db_fname, "metadata")
+        if data_exists and force_update:
+            decay_radiation.drop(isotope_string, axis=0, inplace=True)
+            meta.drop(isotope_string, axis=0, inplace=True)
+
+        decay_radiation = decay_radiation.append(new_decay_radiation)
+        meta = meta.append(new_meta)
+    else:
+        decay_radiation = new_decay_radiation
+        meta = new_meta
+
+    with pd.HDFStore(db_fname, mode="w") as decay_radiation_db:
+        decay_radiation_db["metadata"] = meta
+        decay_radiation_db["decay_radiation"] = decay_radiation
+
+
+def get_decay_radiation_database():
+    """
+    Loads and returns the nuclear decay radiation database and returns the
+    database and metadata
+
+    Returns
+    -------
+    decay_radiation_db: pandas.DataFrame
+    meta: pandas.DataFrame
+    """
+
+    decay_radiation_db = pd.read_hdf(_get_nuclear_database_path(), "decay_radiation")
+    meta = pd.read_hdf(_get_nuclear_database_path(), "metadata")
+
+    return decay_radiation_db, meta
---
+++ carsus/io/nndc/parsers.py
@@ -0,0 +1,114 @@
+from abc import ABCMeta
+
+import pandas as pd
+from astropy import units as u
+from uncertainties import ufloat_fromstr
+import numpy as np
+
+
+def uncertainty_parser(unc_raw_str, split_unc_symbol='%'):
+    unc_raw_str = unc_raw_str.replace('×10+', 'e+')
+    unc_raw_str = unc_raw_str.lower()
+
+    value_unc_pair = [item.strip()
+                  for item in unc_raw_str.split(split_unc_symbol)]
+
+    if len(value_unc_pair) == 1:  # if no uncertainty given
+        return float(value_unc_pair[0]), np.nan
+    else:
+        value, unc = value_unc_pair[:2]  # limit to two
+    if unc == "" or unc == "?":  # if uncertainty_str is empty or unknown
+        unc = np.nan
+    if "e" in value:
+        exp_pos = value.find("e")
+        unc_str = value[:exp_pos] + f"({unc})" + value[exp_pos:]
+    else:
+        unc_str = value + f"({unc})"
+    parsed_uncertainty = ufloat_fromstr(unc_str)
+    return parsed_uncertainty.nominal_value, parsed_uncertainty.std_dev
+
+
+class BaseParser(metaclass=ABCMeta):
+    @staticmethod
+    def _convert_html_to_df(html_table, column_names):
+        df = pd.read_html(str(html_table))[0].iloc[1:]
+        df.columns = column_names
+        if "type" in column_names:
+            df.type[df.type.isnull()] = ""
+        return df
+
+    def _sanititze_table(self, df):
+        df.dropna(inplace=True)
+
+        if "intensity" in df.columns:
+            intensity_raw = df["intensity"].apply(
+                uncertainty_parser)
+            df.loc[:, "intensity"] = [item[0] for item in intensity_raw]
+            df.loc[:, "intensity_unc"] = [item[1]
+                                          for item in intensity_raw]
+        if "energy" in df.columns:
+            energy = df["energy"].apply(
+                uncertainty_parser, split_unc_symbol=' ')
+            df.loc[:, "energy"] = [item[0] for item in energy]
+            df.loc[:, "energy_unc"] = [item[1] for item in energy]
+
+        if "end_point_energy" in df.columns:
+            end_point_energy = df["end_point_energy"].apply(
+                uncertainty_parser, split_unc_symbol=' ')
+            df.loc[:, "end_point_energy"] = [
+                item[0] for item in end_point_energy]
+            df.loc[:, "end_point_energy_unc"] = [
+                item[1] for item in end_point_energy]
+
+        if "dose" in df.columns:
+            del df["dose"]
+        df['heading'] = self.html_name
+        return df
+
+    def _default_parse(self, html_table):
+        df = self._convert_html_to_df(html_table, self.columns)
+        df = self._sanititze_table(df)
+        df['type'] = self.type
+        return df
+
+    def parse(self, html_table):
+        return self._default_parse(html_table)
+
+
+class ElectronTableParser(BaseParser):
+    html_name = "Electrons"
+    type = "e-"
+    columns = ["type", "energy", "intensity", "dose"]
+
+
+
+class BetaPlusTableParser(BaseParser):
+    html_name = "Beta+"
+    type = "e+"
+    columns = ["energy", "end_point_energy", "intensity", "dose"]
+
+
+class BetaMinusTableParser(BaseParser):
+    html_name = "Beta-"
+    type = "e-"
+    columns = ["energy", "end_point_energy", "intensity", "dose"]
+
+
+class XGammaRayParser(BaseParser):
+    html_name = "Gamma and X-ray radiation"
+    columns = ["type", "energy", "intensity", "dose"]
+
+    def parse(self, html_table):
+        df = self._convert_html_to_df(html_table, self.columns)
+        df = self._sanititze_table(df)
+
+        x_ray_mask = df.type.str.startswith("XR")
+
+        df['type'] = 'x_rays'
+        df.loc[~x_ray_mask, 'type'] = 'gamma_rays'
+        return df
+
+
+decay_radiation_parsers = {
+    item.html_name: item() for item in BaseParser.__subclasses__()
+}
